32b69d28291e030a0c81266de54f5fd42ff0ba21
“Hanif <hanifrazin@gmail.com>
2018-02-15 11:42:37 +0700

    Add Class ListPresidentAdapter and Constructor

---
+++ app/src/main/java/com/example/hanif/myrecyclerview/ListPresidentAdapter.java
@@ -0,0 +1,21 @@
+package com.example.hanif.myrecyclerview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+/**
+ * Created by HANIF on 15/02/2018.
+ */
+
+public class ListPresidentAdapter extends RecyclerView.Adapter<ListPresidentAdapter.CategoryViewHolder> {
+    private Context context;
+
+    public ListPresidentAdapter(Context context) {
+        this.context = context;
+    }
+}
