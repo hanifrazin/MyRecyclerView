537b73b45ab970dc7f105515e49734d67aa4794e
“Hanif <hanifrazin@gmail.com>
2018-02-15 12:55:02 +0700

    Add layout item_grid_president.xml and Class GridPresidentAdapter

---
+++ app/src/main/java/com/example/hanif/myrecyclerview/GridPresidentAdapter.java
@@ -0,0 +1,63 @@
+package com.example.hanif.myrecyclerview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+/**
+ * Created by HANIF on 15/02/2018.
+ */
+
+public class GridPresidentAdapter extends RecyclerView.Adapter<GridPresidentAdapter.GridViewHolder> {
+    private Context context;
+    private ArrayList<President> listPresident;
+
+    public GridPresidentAdapter(Context context) {
+        this.context = context;
+    }
+
+    public ArrayList<President> getListPresident() {
+        return listPresident;
+    }
+
+    public void setListPresident(ArrayList<President> listPresident) {
+        this.listPresident = listPresident;
+    }
+
+
+    @Override
+    public GridPresidentAdapter.GridViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_grid_president, parent, false);
+        GridViewHolder gridViewHolder = new GridViewHolder(view);
+        return gridViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(GridPresidentAdapter.GridViewHolder holder, int position) {
+        Glide.with(context)
+                .load(getListPresident().get(position).getPhoto())
+                .override(350, 550)
+                .into(holder.imgPhoto);
+    }
+
+    @Override
+    public int getItemCount() {
+        return getListPresident().size();
+    }
+
+    public class GridViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgPhoto;
+        public GridViewHolder(View itemView) {
+            super(itemView);
+            imgPhoto = (ImageView)itemView.findViewById(R.id.img_item_photo);
+        }
+    }
+}
