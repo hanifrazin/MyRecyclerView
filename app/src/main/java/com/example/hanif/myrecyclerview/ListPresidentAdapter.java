3336b9a980d04aed64799c5f90876a406710cc03
“Hanif <hanifrazin@gmail.com>
2018-02-15 11:57:16 +0700

    Add Sub Class CategoryViewHolder in Class ListPresidentAdapter

--- app/src/main/java/com/example/hanif/myrecyclerview/ListPresidentAdapter.java
+++ app/src/main/java/com/example/hanif/myrecyclerview/ListPresidentAdapter.java
@@ -1,10 +1,13 @@
 package com.example.hanif.myrecyclerview;
 
 import android.content.Context;
+import android.provider.ContactsContract;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -27,4 +30,19 @@ public class ListPresidentAdapter extends RecyclerView.Adapter<ListPresidentAdap
     public void setListPresident(ArrayList<ListPresidentAdapter> listPresident) {
         this.listPresident = listPresident;
     }
+
+    public class CategoryViewHolder extends RecyclerView.ViewHolder {
+        TextView tvName;
+        TextView tvRemarks;
+        ImageView imgPhoto;
+
+        public CategoryViewHolder(View itemView) {
+            super(itemView);
+            tvName = (TextView)itemView.findViewById(R.id.tv_item_name);
+            tvRemarks = (TextView)itemView.findViewById(R.id.tv_item_remarks);
+            imgPhoto = (ImageView)itemView.findViewById(R.id.img_item_photo);
+        }
+    }
+
+
 }
